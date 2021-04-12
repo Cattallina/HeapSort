cdee600b333e5dd528d39f164df4e8393d6c2c30
Trina <cattallina017@gmail.com>
2021-04-12 22:10:52 +0300

    commit

---
+++ script.js
@@ -0,0 +1,314 @@
+//let array = [{900:'A1'},{1500:'B1'},{1800:'B2'},{20000:'B3'},{425000:'B4'},{5000:'B5'},{2000:'B6'},{50:'B7'},{500:'B8'},{400:'B9'},{300000:'B11'},{3:'B12'},{1000:'B13'},{150000:'B15'},{90000:'C'},{15:'D1-D5'},{15000:'E'},{120:'K1,K2'},{30000:'N'}];
+
+let arr = [
+	{
+		dose: 900,
+		vitamin: 'A1'
+	},
+
+	{
+		dose: 1500,
+		vitamin: 'B1'
+	},
+
+	{
+		dose: 1800,
+		vitamin: 'B2'
+	},
+
+	{
+		dose: 20000,
+		vitamin: 'B3'
+	},
+
+	{
+		dose: 425000,
+		vitamin: 'B4'
+	},
+
+	{
+		dose: 5000,
+		vitamin: 'B5'
+	},
+
+	{
+		dose: 2000,
+		vitamin: 'B6'
+	},
+
+	{
+		dose: 50,
+		vitamin: 'B7'
+	},
+
+	{
+		dose: 500,
+		vitamin: 'B8'
+	},
+
+	{
+		dose: 400,
+		vitamin: 'B9'
+	},
+
+	{
+		dose: 300000,
+		vitamin: 'B11'
+	},
+
+	{
+		dose: 3,
+		vitamin: 'B12'
+	},
+
+	{
+		dose: 1000,
+		vitamin: 'B13'
+	},
+
+	{
+		dose: 150000,
+		vitamin: 'B15'
+	},
+
+	{
+		dose: 90000,
+		vitamin: 'C'
+	},
+
+	{
+		dose: 15,
+		vitamin: 'D1'
+	},
+
+	{
+		dose: 15000,
+		vitamin: 'E'
+	},
+
+	{
+		dose: 120,
+		vitamin: 'K1'
+	},
+
+	{
+		dose: 30000,
+		vitamin: 'N'
+	}
+];
+
+function heapify(arr, length, j) {
+	let largest = j;
+	let left = j * 2 + 1;
+	let right = left + 1;
+
+	if (left < length && arr[left].dose > arr[largest].dose) {
+		largest = left;
+	}
+
+	if (right < length && arr[right].dose > arr[largest].dose) {
+		largest = right;
+	}
+
+	if (largest !=j) {
+		[arr[j],arr[largest]] = [arr[largest], arr[j]];
+		heapify(arr, length, largest)
+	}
+
+	return arr;
+}
+
+function heapSort(arr) {
+	let length = arr.length;
+	let j = Math.floor(length / 2 - 1);
+	let k = length - 1;
+
+	while (j >= 0) {
+		heapify(arr, length, j);
+		j--;
+	}
+
+	while (k >= 0) {
+		[arr[0], arr[k]] = [arr[k], arr[0]];
+		heapify(arr, k, 0);
+		k--;
+	}
+	return arr;
+}
+
+console.log(heapSort(arr));
+
+const input = document.createElement('div');
+	  input.innerHTML = `
+	    <div class="search-elem">
+		    <span>Найти элемент</span>
+		    <input type="text" id="input" placeholder="Enter vitamin name"> --
+		    <input type="text" id="znach" placeholder="">
+		    <button id="btn">Click</button>
+		    <button id="btn2">Delite</button>
+		    <button id="btn3">Push</button>
+	    </div>
+	    
+	    <div class="wrapper">
+	    	<div class="daily-rate">
+	    		<span class="name-tabl">Суточная норма</span>
+				   	<p id="rate1" class="rate"></p>
+				   	<hr>
+				   	<p id="rate2" class="rate"></p>
+				   	<hr>
+				   	<p id="rate3" class="rate"></p>
+				   	<hr>
+				   	<p id="rate4" class="rate"></p>
+				   	<hr>
+				   	<p id="rate5" class="rate"></p>
+				   	<hr>
+				   	<p id="rate6" class="rate"></p>
+				   	<hr>
+				   	<p id="rate7" class="rate"></p>
+				   	<hr>
+				   	<p id="rate8" class="rate"></p>
+				   	<hr>
+				   	<p id="rate9" class="rate"></p>
+				   	<hr>
+				   	<p id="rate10" class="rate"></p>
+				   	<hr>
+				   	<p id="rate11" class="rate"></p>
+				   	<hr>
+				   	<p id="rate12" class="rate"></p>
+				   	<hr>
+				   	<p id="rate13" class="rate"></p>
+				   	<hr>
+				   	<p id="rate14" class="rate"></p>
+				   	<hr>
+				   	<p id="rate15" class="rate"></p>
+				   	<hr>
+				   	<p id="rate16" class="rate"></p>
+				   	<hr>
+				   	<p id="rate17" class="rate"></p>
+				   	<hr>
+				   	<p id="rate18" class="rate"></p>
+				   	<hr>
+				   	<p id="rate19" class="rate"></p>
+				   	<hr>
+			</div>
+			<div class="name-vitamin" id="name-vitamins">
+				<span class="name-tabl">Название витаминов</span>
+				   	<p class="name-vit" id="name-vit1"></p>
+				   	<p class="name-vit" id="name-vit2"></p>
+				   	<p class="name-vit" id="name-vit3"></p>
+				   	<p class="name-vit" id="name-vit4"></p>
+				   	<p class="name-vit" id="name-vit5"></p>
+				   	<p class="name-vit" id="name-vit6"></p>
+				   	<p class="name-vit" id="name-vit7"></p>
+				   	<p class="name-vit" id="name-vit8"></p>
+				   	<p class="name-vit" id="name-vit9"></p>
+				   	<p class="name-vit" id="name-vit10"></p>
+				   	<p class="name-vit" id="name-vit11"></p>
+				   	<p class="name-vit" id="name-vit12"></p>
+				   	<p class="name-vit" id="name-vit13"></p>
+				   	<p class="name-vit" id="name-vit14"></p>
+				   	<p class="name-vit" id="name-vit15"></p>
+				   	<p class="name-vit" id="name-vit16"></p>
+				   	<p class="name-vit" id="name-vit17"></p>
+				   	<p class="name-vit" id="name-vit18"></p>
+				   	<p class="name-vit" id="name-vit19"></p>
+			</div>
+		</div>
+	   	`;
+document.body.prepend(input);
+
+const rate = document.querySelectorAll('.rate'),
+	  vit = document.querySelectorAll('.name-vit'),
+	  input1 = document.querySelector('#input'),
+	  input2 = document.querySelector('#znach'),
+	  btn = document.querySelector('#btn'),
+	  btn2 = document.querySelector('#btn2'),
+	  btn3 = document.querySelector('#btn3');
+
+const rate1 = document.querySelectorAll(`#rate1`),
+	  rate2 = document.querySelectorAll(`#rate2`),
+	  rate3 = document.querySelectorAll(`#rate3`),
+	  rate4 = document.querySelectorAll(`#rate4`),
+	  rate5 = document.querySelectorAll(`#rate5`),
+	  rate6 = document.querySelectorAll(`#rate6`),
+	  rate7 = document.querySelectorAll(`#rate7`),
+	  rate8 = document.querySelectorAll(`#rate8`),
+	  rate9 = document.querySelectorAll(`#rate9`),
+	  rate10 = document.querySelectorAll(`#rate10`),
+	  rate11 = document.querySelectorAll(`#rate11`),
+	  rate12 = document.querySelectorAll(`#rate12`),
+	  rate13 = document.querySelectorAll(`#rate13`),
+	  rate14 = document.querySelectorAll(`#rate14`),
+	  rate15 = document.querySelectorAll(`#rate15`),
+	  rate16 = document.querySelectorAll(`#rate16`),
+	  rate17 = document.querySelectorAll(`#rate17`),
+	  rate18 = document.querySelectorAll(`#rate18`),
+	  rate19 = document.querySelectorAll(`#rate19`),
+	  vit1 = document.querySelectorAll(`#name-vit1`),
+	  vit2 = document.querySelectorAll(`#name-vit2`),
+	  vit3 = document.querySelectorAll(`#name-vit3`),
+	  vit4 = document.querySelectorAll(`#name-vit4`),
+	  vit5 = document.querySelectorAll(`#name-vit5`),
+	  vit6 = document.querySelectorAll(`#name-vit6`),
+	  vit7 = document.querySelectorAll(`#name-vit7`),
+	  vit8 = document.querySelectorAll(`#name-vit8`),
+	  vit9 = document.querySelectorAll(`#name-vit9`),
+	  vit10 = document.querySelectorAll(`#name-vit10`),
+	  vit11 = document.querySelectorAll(`#name-vit11`),
+	  vit12 = document.querySelectorAll(`#name-vit12`),
+	  vit13 = document.querySelectorAll(`#name-vit13`),
+	  vit14 = document.querySelectorAll(`#name-vit14`),
+	  vit15 = document.querySelectorAll(`#name-vit15`),
+	  vit16 = document.querySelectorAll(`#name-vit16`),
+	  vit17 = document.querySelectorAll(`#name-vit17`),
+	  vit18 = document.querySelectorAll(`#name-vit18`),
+	  vit19 = document.querySelectorAll(`#name-vit19`);
+
+
+function createTabl(){
+	for (let i = 0; i < arr.length; i++) {
+		rate[i].innerHTML = arr[i].dose;
+		vit[i].innerHTML = arr[i].vitamin;
+	};
+}
+createTabl();
+
+function getEvent(){
+	btn.addEventListener('click', ()=>{	
+		input2.value = arr.find(nam => nam.vitamin === input1.value).dose + 'mkg';
+	});
+
+	btn2.addEventListener('click', ()=>{	
+		let xx = arr.find(nam => nam.vitamin === input1.value);
+		arr = arr.filter(item => item !== xx);
+
+		for(let i = vit.length - 1; 0 <= i; i--)
+ 			if (vit[i] && vit[i].parentElement){
+ 				vit[i].parentElement.removeChild(vit[i]);
+			}
+
+		console.log(arr);
+	});
+
+	btn3.addEventListener('click', ()=>{	
+		arr.push({
+			dose: +input2.value,
+			vitamin: input1.value
+		});
+		console.log(arr);
+	
+		for(let i = vit.length - 1; 0 <= i; i--)
+ 			if (vit[i] && vit[i].parentElement){
+ 				vit[i].parentElement.innerHTML += '<hr>' + input1.value ;
+			}
+
+		for(let j = rate.length - 1; 0 <= j; j--)
+ 			if (rate[j] && rate[j].parentElement){
+ 				rate[j].parentElement.innerHTML += input2.value;
+			}
+		let dd = document.querySelector('.name-vitamin');
+		dd.style.marginTop = '-494px';
+	});
+}
+
+getEvent();
